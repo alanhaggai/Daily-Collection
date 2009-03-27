9afb51ce8cad4b22886c0b4d84b4b8b6b64f41b0
Alan Haggai Alavi <alanhaggai@alanhaggai.org>
2009-03-27 15:34:14 +0530

    Populated tableWidget with respective values

--- daybookagentdialog.cpp
+++ daybookagentdialog.cpp
@@ -4,7 +4,13 @@
 
 #include "daybookagentdialog.h"
 
-DaybookAgentDialog::DaybookAgentDialog(QDialog *parent) : QDialog(parent) {
+const int SERIAL  = 0;
+const int NAME    = 1;
+const int AMOUNT  = 2;
+const int PAID    = 3;
+const int BALANCE = 4;
+
+DaybookAgentDialog::DaybookAgentDialog(QDialog *parent) : QDialog( parent, Qt::Tool ) {
     setupUi(this);
 
     QSqlQuery query;
@@ -18,4 +24,62 @@ DaybookAgentDialog::DaybookAgentDialog(QDialog *parent) : QDialog(parent) {
          agentMap[query.value(0).toInt()] = query.value(1).toString();
     }
     agentCombo->setCurrentIndex(-1);
+
+    tableWidget->setColumnWidth( SERIAL,  50);
+    tableWidget->setColumnWidth( NAME,    200);
+    tableWidget->setColumnWidth( AMOUNT,  70);
+    tableWidget->setColumnWidth( PAID,    70);
+    tableWidget->setColumnWidth( BALANCE, 70);
+
+    connect( agentCombo, SIGNAL(currentIndexChanged(int)), this, SLOT(populateTableWidget(int)) );
+}
+
+void DaybookAgentDialog::populateTableWidget(int currentIndex) {
+    int agentId = agentMap.key( agentCombo->itemText(currentIndex) );
+
+    QSqlQuery query;
+    query.prepare("SELECT debtor.serial, debtor.name, debtor.amount, SUM(transaction.paid) FROM debtor, transaction WHERE debtor.agent_id = :agentId AND debtor.id = transaction.debtor_id GROUP BY debtor.serial ORDER BY debtor.serial");
+    query.bindValue( ":agent_id", agentId );
+
+    if ( !query.exec() ) {
+        qDebug() << query.lastError();
+        qFatal("Failed to execute query.");
+    }
+
+    tableWidget->setRowCount(query.size());
+
+    int row = 0;
+
+    while ( query.next() ) {
+        QString debtorSerial  = query.value(SERIAL).toString();
+        QString debtorName    = query.value(NAME).toString();
+        QString debtorAmount  = query.value(AMOUNT).toString();
+        QString debtorPaid    = query.value(PAID).toString();
+        QString debtorBalance = QString::number( query.value(AMOUNT).toInt() - query.value(PAID).toInt() );
+        //QString debtorBalance = "";
+
+        QTableWidgetItem *serialItem  = new QTableWidgetItem;
+        QTableWidgetItem *nameItem    = new QTableWidgetItem;
+        QTableWidgetItem *amountItem  = new QTableWidgetItem;
+        QTableWidgetItem *paidItem    = new QTableWidgetItem;
+        QTableWidgetItem *balanceItem = new QTableWidgetItem;
+
+        qDebug() << "DaybookAgentDialog::populateTableWidget() - " << "Serial: "  + debtorSerial;
+        qDebug() << "DaybookAgentDialog::populateTableWidget() - " << "Name: "    + debtorName;
+        qDebug() << "DaybookAgentDialog::populateTableWidget() - " << "Amount: "  + debtorAmount;
+        qDebug() << "DaybookAgentDialog::populateTableWidget() - " << "Paid: "    + debtorPaid;
+        qDebug() << "DaybookAgentDialog::populateTableWidget() - " << "Balance: " + debtorBalance;
+
+        serialItem->setText(debtorSerial);
+        nameItem->setText(debtorName);
+        amountItem->setText(debtorAmount);
+        paidItem->setText(debtorPaid);
+        balanceItem->setText(debtorBalance);
+
+        tableWidget->setItem( row,   SERIAL,  serialItem );
+        tableWidget->setItem( row,   NAME,    nameItem );
+        tableWidget->setItem( row,   AMOUNT,  amountItem );
+        tableWidget->setItem( row,   PAID,    paidItem );
+        tableWidget->setItem( row++, BALANCE, balanceItem );
+    }
 }
--- daybookagentdialog.h
+++ daybookagentdialog.h
@@ -4,8 +4,13 @@
 #include "ui_daybookagentdialog.h"
 
 class DaybookAgentDialog : public QDialog, public Ui::DaybookAgentDialog {
+    Q_OBJECT
+
     public:
         DaybookAgentDialog( QDialog *parent = 0 );
+
+    public slots:
+        void populateTableWidget(int);
 
     private:
         QMap<int, QString> agentMap;
